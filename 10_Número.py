3bcea9634d98d49f84632ad179ad0ef33a9daaa7
RoqueFM <71264475+RoqueFM@users.noreply.github.com>
2020-11-04 09:39:39 +0100

    Add files via upload

---
+++ 10_Número.py
@@ -0,0 +1,8 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def numero():
+	n=input("Inserte número: ")
+	print n-3, n-2, n-1, n, n+1, n+2, n+3
+
+	
+>>> numero()
